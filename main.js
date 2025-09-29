9bdf926f7d28d51bfe71e9c1edfd8ae90d9809d4
Дубик Олександр bc22025-3 <uradubik66@gmail.com>
2025-09-30 00:29:50 +0300

    Було завершено програму main,js, також полагодив house-price.json(неправильно записаний json) та зберіг результат у output.txt

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 const {program} = require('commander');
-const {fs}= require('fs');
+const fs= require('fs');
 
 program
  .requiredOption('-i, --input <path>', 'Шлях до файлу для читання')
  .option('-o, --output <path>', 'Шлях до файлу для запису результату')
- .option('-d, --display', 'Виведення результату в консоль');
-
+ .option('-d, --display', 'Виведення результату в консоль')
+ .option('-f, --furnished', 'Відображати лише будинки зі статусом "furnished" - з меблями')
+ .option('-p, --price <value>', 'Відображати лише будинки з ціною меншою за зазначену', parseFloat);
 program.parse();
 
 const options = program.opts();
@@ -21,12 +22,28 @@ if(!fs.existsSync(options.input)){
  
  const content = fs.readFileSync(options.input, 'utf-8');
 
- data = JSON.parse(content);
+ const data = JSON.parse(content);
  
- const result = JSON.stringify(data, null, 2);
+
+
+let FilteredData=data;
+
+if(options.furnished){
+    FilteredData=FilteredData.filter(item=>item.furnishingstatus && item.furnishingstatus === 'furnished');
+}
+if(options.price !== undefined){
+    FilteredData=FilteredData.filter(item=>item.price && item.price < options.price);
+}
+
+
+
+
+ const result = FilteredData
+ .map(item=>`${item.price} ${item.area}`)
+ .join("\n");
 
 if(options.output){
-   fs.writefilesync(options.output, result, "utf-8");
+   fs.writeFileSync(options.output, result, "utf-8");
 }
 if(options.display){
     console.log(result);
